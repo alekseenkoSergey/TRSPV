6f6ed3f0397259d0f24d5c0eeb0b000796c73459
Sergey Alekseenko <alekseenko365@gmail.com>
2017-03-15 12:41:01 +0200

    Begin pr1

---
+++ src/pr1/MyThread.java
@@ -0,0 +1,21 @@
+package pr1;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Sergey on 13.03.2017.
+ */
+public class MyThread implements Runnable {
+    ArrayList<Integer> array;
+    int k;
+
+    public MyThread(ArrayList<Integer> array, int k) {
+        this.array = array;
+        this.k = k;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
